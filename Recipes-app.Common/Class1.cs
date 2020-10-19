028b32584aaa7bb3f4734caa5ca014116bef5738
Veronica Kolarska <veronica.kolarska@gmail.com>
2020-10-19 16:56:21 +0300

    refactor: project to recipes

--- Recipes-app.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Recipes_app.Common
-{
-    public class Class1
-    {
-    }
-}
--- Recipes-app.Common/Constants/GlobalConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace Recipes_app.Common.Constants
-{
-    public class GlobalConstants
-    {
-        
-    }
-}
