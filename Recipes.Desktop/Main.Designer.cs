aeb1d02bc6a5e7c2d0ef37b81d19d3d9dbe79e9c
Veronica Kolarska <veronica.kolarska@gmail.com>
2020-12-22 12:47:49 +0200

    feat: added some tabs

--- Recipes.Desktop/Main.Designer.cs
+++ Recipes.Desktop/Main.Designer.cs
@@ -29,13 +29,61 @@ namespace Recipes.Desktop
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.loginPage = new System.Windows.Forms.TabPage();
+            this.registerPage = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.loginPage);
+            this.tabControl1.Controls.Add(this.registerPage);
+            this.tabControl1.Location = new System.Drawing.Point(217, 65);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(71, 3);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(365, 255);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // loginPage
+            // 
+            this.loginPage.Location = new System.Drawing.Point(4, 24);
+            this.loginPage.Name = "loginPage";
+            this.loginPage.Padding = new System.Windows.Forms.Padding(3);
+            this.loginPage.Size = new System.Drawing.Size(357, 227);
+            this.loginPage.TabIndex = 0;
+            this.loginPage.Text = "Login";
+            this.loginPage.UseVisualStyleBackColor = true;
+            // 
+            // registerPage
+            // 
+            this.registerPage.Location = new System.Drawing.Point(4, 24);
+            this.registerPage.Name = "registerPage";
+            this.registerPage.Padding = new System.Windows.Forms.Padding(3);
+            this.registerPage.Size = new System.Drawing.Size(378, 227);
+            this.registerPage.TabIndex = 1;
+            this.registerPage.Text = "Register";
+            this.registerPage.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Main";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage loginPage;
+        private System.Windows.Forms.TabPage registerPage;
     }
 }
 
